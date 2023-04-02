91c36ebe9f48f326113450c88fd88f63e70e5ef3
Ken Chen <chengusky@gmail.com>
2023-04-02 08:23:29 +0800

    Update daily_hot_news.py to use worker

--- app/daily_hot_news.py
+++ app/daily_hot_news.py
@@ -4,6 +4,7 @@ import logging
 import feedparser
 import html2text
 import concurrent.futures
+import requests
 
 from app.gpt import get_answer_from_llama_web
 
@@ -13,7 +14,7 @@ with open("app/data/hot_news_rss.json", "r") as f:
 TODAY = today = date.today()
 MAX_DESCRIPTION_LENGTH = 300
 MAX_POSTS = 3
-
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
 
 def cut_string(text):
     words = text.split()
@@ -32,9 +33,9 @@ def get_summary_from_gpt_thread(url):
     return str(get_answer_from_llama_web([news_summary_prompt], [url]))
 
 def get_summary_from_gpt(url):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future = executor.submit(get_summary_from_gpt_thread, url)
-        return future.result(timeout=300)
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    future = executor.submit(get_summary_from_gpt_thread, url)
+    return future.result(timeout=300)
 
 def get_description(entry):
     gpt_answer = None
@@ -45,7 +46,7 @@ def get_description(entry):
     if gpt_answer is not None:
         summary = 'AI: ' + gpt_answer
     else:
-        summary = cut_string(get_text_from_html(entry.summary))
+        summary = cut_string(get_text_from_html(entry.description))
     return summary
 
 def get_text_from_html(html):
@@ -55,24 +56,47 @@ def get_text_from_html(html):
     text_maker.ignore_images = True
     return text_maker.handle(html)
 
-def get_post_urls_with_title(rss_url):
-    feed = feedparser.parse(rss_url)
-    updated_posts = []
-    
-    for entry in feed.entries:
-        published_time = entry.published_parsed if 'published_parsed' in entry else None
-        # published_date = date(published_time.tm_year,
-        #                       published_time.tm_mon, published_time.tm_mday)
-        updated_post = {}
-        updated_post['title'] = entry.title
-        updated_post['summary'] = get_description(entry)
-        updated_post['url'] = entry.link
-        updated_post['publish_date'] = published_time
-        updated_posts.append(updated_post)
-        if len(updated_posts) >= MAX_POSTS:
-            break
+def get_post_urls_with_title(rss_url: str):
+    headers = {'Accept': 'application/json'}
+    endpoint_url = f"https://rss-worker.thinkingincrowd.workers.dev/?url={rss_url}"
+    response = requests.get(endpoint_url, headers=headers)
+    if response.status_code == 200:
+        try:
+            feed = response.json()
+            updated_posts = []
+            for entry in feed.items:
+                updated_post = {}
+                updated_post['title'] = entry.title
+                updated_post['summary'] = get_description(entry)
+                updated_post['url'] = entry.link
+                updated_post['publish_date'] = entry.pubDate
+                updated_posts.append(updated_post)
+                if len(updated_posts) >= MAX_POSTS:
+                    break
+            return updated_posts
+        except:
+            return "Error: Unable to get rss json content"
+    else:
+        return f"Error: {response.status_code} - {response.reason}"
+
+# def get_post_urls_with_title(rss_url):
+#     feed = feedparser.parse(rss_url)
+#     updated_posts = []
+
+#     for entry in feed.entries:
+#         published_time = entry.published_parsed if 'published_parsed' in entry else None
+#         # published_date = date(published_time.tm_year,
+#         #                       published_time.tm_mon, published_time.tm_mday)
+#         updated_post = {}
+#         updated_post['title'] = entry.title
+#         updated_post['summary'] = get_description(entry)
+#         updated_post['url'] = entry.link
+#         updated_post['publish_date'] = published_time
+#         updated_posts.append(updated_post)
+#         if len(updated_posts) >= MAX_POSTS:
+#             break
         
-    return updated_posts
+#     return updated_posts
 
 def build_slack_blocks(title, news):
     blocks = [
@@ -139,25 +163,24 @@ def build_jisilu_news_hot_news_blocks():
     return build_hot_news_blocks('jisilu')
 
 def build_all_news_block():
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        zhihu_news = executor.submit(build_zhihu_hot_news_blocks)
-        v2ex_news = executor.submit(build_v2ex_hot_news_blocks)
-        onepoint3acres_news = executor.submit(build_1point3acres_hot_news_blocks)
-        reddit_news = executor.submit(build_reddit_news_hot_news_blocks)
-        hackernews_news = executor.submit(build_hackernews_news_hot_news_blocks)
-        producthunt_news = executor.submit(build_producthunt_news_hot_news_blocks)
-        xueqiu_news = executor.submit(build_xueqiu_news_hot_news_blocks)
-        jisilu_news = executor.submit(build_jisilu_news_hot_news_blocks)
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    zhihu_news = executor.submit(build_zhihu_hot_news_blocks)
+    v2ex_news = executor.submit(build_v2ex_hot_news_blocks)
+    onepoint3acres_news = executor.submit(build_1point3acres_hot_news_blocks)
+    reddit_news = executor.submit(build_reddit_news_hot_news_blocks)
+    hackernews_news = executor.submit(build_hackernews_news_hot_news_blocks)
+    producthunt_news = executor.submit(build_producthunt_news_hot_news_blocks)
+    # xueqiu_news = executor.submit(build_xueqiu_news_hot_news_blocks)
+    # jisilu_news = executor.submit(build_jisilu_news_hot_news_blocks)
 
-        zhihu_news_block = zhihu_news.result()
-        v2ex_news_block = v2ex_news.result()
-        onepoint3acres_news_block = onepoint3acres_news.result()
-        reddit_news_block = reddit_news.result()
-        hackernews_news_block = hackernews_news.result()
-        producthunt_news_block = producthunt_news.result()
-        xueqiu_news_block = xueqiu_news.result()
-        jisilu_news_block = jisilu_news.result()
+    zhihu_news_block = zhihu_news.result()
+    v2ex_news_block = v2ex_news.result()
+    onepoint3acres_news_block = onepoint3acres_news.result()
+    reddit_news_block = reddit_news.result()
+    hackernews_news_block = hackernews_news.result()
+    producthunt_news_block = producthunt_news.result()
+    # xueqiu_news_block = xueqiu_news.result()
+    # jisilu_news_block = jisilu_news.result()
 
-        return [zhihu_news_block, v2ex_news_block, onepoint3acres_news_block,
-                           reddit_news_block, hackernews_news_block, producthunt_news_block,
-                           xueqiu_news_block, jisilu_news_block]
+    return [zhihu_news_block, v2ex_news_block, onepoint3acres_news_block,
+                        reddit_news_block, hackernews_news_block, producthunt_news_block]
